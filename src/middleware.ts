dbbe0fe0643a804c6b11a432f4ec755af6abc1a1
ivanxaraloba <ivanxara@loba.com>
2025-02-24 16:54:54 +0000

    fix: middlware ( remove '/' path )

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,8 +13,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - / (root)
      * - /login and /auth/callback (login and auth callback)
      */
-    '/((?!_next/static|_next/image|favicon.ico|login|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|login|auth/callback|^$|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
