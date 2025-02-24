066e5e98fce4ad950b16a9fce2ace9ef7acaba89
ivanxaraloba <ivanxara@loba.com>
2025-02-24 16:54:54 +0000

    dev: test auth 4

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -40,11 +40,11 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth/callback')
+    !request.nextUrl.pathname.startsWith('/auth/callback') &&
+    request.nextUrl.pathname !== '/'
   ) {
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = '/sign-up';
+    url.pathname = '/login';
     return NextResponse.redirect(url);
   }
 
