3a5e4aa388ad72aee1248eee24b06f2281d6401d
ivanxaraloba <ivanxara@loba.com>
2025-02-24 16:54:54 +0000

    fix: remove middleware in api routes

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -41,6 +41,7 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth/callback') &&
+    !request.nextUrl.pathname.startsWith('/api') &&
     request.nextUrl.pathname !== '/'
   ) {
     const url = request.nextUrl.clone();
